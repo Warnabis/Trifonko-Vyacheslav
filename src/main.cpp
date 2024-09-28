05ef3aaf6aa38866aaa76f7d82864f48f5354196
Trifonko V. <vtrifonko@gmail.com>
2024-09-28 19:44:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,53 @@
 #include "Subscription.h"
-#include <locale.h>
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int chooseOption() {
+    int choice;
+    cout << "\n1. Создать новый объект\n2. Вывести объект(ы)\n3. Обновить объект(ы)\n4. Удалить объект(ы)\n5. Управление тренировками (выбор, тренировка, статус)\n6. Выйти из программы\nВыберите опцию: ";
+    cin >> choice;
+    cout << endl;
+    return choice;
+}
 
 int main() {
     setlocale(LC_ALL, "rus");
-    Subscription service;
-    Subscription* services = nullptr;
+
     int size = 0;
+    unique_ptr<Subscription[]> services(new Subscription[0]);
+    Subscription service;
     Subscription* selectedservice = nullptr;
 
     int choice;
     do {
-        std::cout << "Меню:\n"
-            << "1. Создать услугу\n"
-            << "2. Показать услуги\n"
-            << "3. Обновить услугу\n"
-            << "4. Удалить услугу\n"
-            << "5. Тренировка\n"
-            << "6. Выход\n"
-            << "Выберите опцию: ";
+        choice = chooseOption();
 
-        while (!(std::cin >> choice)) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Неверный выбор. Попробуйте снова\nВыберите опцию: ";
+        switch (choice) {
+        case 1:
+            service.create(services, size);
+            break;
+        case 2:
+            service.read(services, size);
+            break;
+        case 3:
+            service.update(services, size);
+            break;
+        case 4:
+            service.deletes(services, size);
+            break;
+        case 5:
+            service.workout(services, size, selectedservice);
+            break;
+        case 6:
+            cout << "Выход из программы..." << endl;
+            break;
+        default:
+            cout << "Неверный выбор. Попробуйте снова" << endl;
+            break;
         }
-
-        choose(choice, service, services, size, selectedservice);
     } while (choice != 6);
 
-    delete[] services; 
     return 0;
 }
