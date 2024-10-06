0fbd7019128b61fda3673d9de885ca24355dfef2
Trifonko V. <vtrifonko@gmail.com>
2024-10-06 19:50:50 +0300

    Update Subscription.h

--- src/Subscription.h
+++ src/Subscription.h
@@ -3,23 +3,34 @@
 
 #include <memory>
 #include <string>
+#include <fstream>
 
 class Subscription {
-public:
     int id;
-    std::string name;
     float price;
     int days;
 
+public:
+    std::string name;
 
     void input();
-    void output() const;
+    friend void output(const Subscription& service);
 
     void create(std::unique_ptr<Subscription[]>& services, int& size) const;
     void read(const Subscription* services, int size) const;
     void update(Subscription* services, int size) const;
     void deletes(std::unique_ptr<Subscription[]>& services, int& size) const;
     void workout(Subscription* services, int size, Subscription*& selectedservice) const;
+    void compareprices(Subscription* services, int size);
+
+    void saveToFile(std::ofstream& ofs) const;
+
+    void loadFromFile(std::ifstream& ifs);
+
+    bool operator==(const Subscription& other) const;
+    bool operator>(const Subscription& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Subscription& sub);
+
 };
 
 #endif
